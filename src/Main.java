aa971b9888272f4ee9cdbd76e00963bf31423e3a
Alexandr <0231050@mail.ru>
2025-09-18 22:24:28 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        int balans = 500;
-        int prihod = 1600;
-        if (prihod >= 1000) {
-            System.out.println("Итого=" + (balans + prihod + prihod / 100) + "  Бонусы=" + (prihod / 100));
+        int balance = 500;
+        int coming = 1600;
+        if (coming >= 1000) {
+            System.out.println("Итого=" + (balance + coming + coming / 100) + "  Бонусы=" + (coming / 100));
         } else {
-            System.out.println("Итого=" + (balans + prihod));
+            System.out.println("Итого=" + (balance + coming));
         }
 
 
